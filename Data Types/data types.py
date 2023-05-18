e83e6156d5d9a2e6624d009d26e28aee968027d4
Tony Nguyen <tbnktd@umsystem.edu>
2023-05-17 22:26:32 -0500

    Create data types.py

---
+++ Data Types/data types.py
@@ -0,0 +1,11 @@
+#data types
+
+#integer are whole numbers
+num = 45
+#float are numbers with decimals 
+nums = 2.232
+#strings a string of characters
+name = "Toby"
+#string concatenation
+#add string to string to create new string 
+print("Hello " + name)
