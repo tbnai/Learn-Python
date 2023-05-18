b35cbc1b8f91e680410ef2e7680e698f645d4a2f
Tony Nguyen <tbnktd@umsystem.edu>
2023-05-18 09:43:13 -0500

    Create MATHS.py

---
+++ Maths/MATHS.py
@@ -0,0 +1,10 @@
+#MATHS 
+
+#Arithmetic Operators 
+#You can do mathematical calculations with python as long as you know the right operators 
+3+3     #Add
+4-1     #Subtract
+2*5     #Multiply 
+4/76    #Divide 
+5**4    #Exponent
+
