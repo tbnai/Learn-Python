6e0f4500b925543c30c6e7bff8469a109ba1f5d6
Tony Nguyen <tbnktd@umsystem.edu>
2023-05-17 23:04:01 -0500

    Update data types.py

--- Data Types/data types.py
+++ Data Types/data types.py
@@ -1,11 +1,35 @@
 #data types
 
-#integer are whole numbers
+#--integer are whole numbers--
 num = 45
-#float are numbers with decimals 
+
+#--float are numbers with decimals --
 nums = 2.232
-#strings a string of characters
+
+#--strings a string of characters--
 name = "Toby"
-#string concatenation
+
+#--string concatenation--
 #add string to string to create new string 
 print("Hello " + name)
+
+#--escaping a string--
+#because the double quotes is special it denotes a string if you want to use it in a string you need to escape it with a "\"
+speech = "Lauren said: \"Hi\""
+print(speech)
+
+#--F-Strings--
+#You can insert a variable into a string using f-string
+day = 240
+print(f"There are {day} in a year")
+
+#--Converting Data Types --
+#You can convert a variable from 1 data type to another 
+n = 354
+new_n = float(n)
+print(new_n)
+
+#--Checking Data Types --
+#You can use the type() function to check what is the data type of a particular variable 
+v = 2.35
+print(type(v))
