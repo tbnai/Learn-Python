2b44cdfafc93c7cd03ee2d9336d3bb65b4870ebe
Tony Nguyen <tbnktd@umsystem.edu>
2023-05-17 20:26:53 -0500

    Input

---
+++ Input/Input.py
@@ -0,0 +1,9 @@
+#---Inputing---#
+
+name = input("Hello what is your name?: ")
+age = int(input("What is your age?: "))
+height = float(input("What is your height?: "))
+
+print("Hello " + name)
+print("Your age is " + str(age))
+print("Your height is " + str(height))
--- Print/Print.py
+++ Print/Print.py
@@ -1,2 +1,3 @@
+#---This is how you print---#
 print("Hello World")
 print("What a world this is..")
